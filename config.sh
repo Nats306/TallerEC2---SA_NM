8be60a8ca5bb0ab00770c8334123ca19bb6648c5
sofiaaristizabal <sofiaaristizabal23@gmail.com>
2025-11-05 11:25:06 -0500

    changed the config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 
 mkdir fastapi
 cd fastapi
-curl 
+git clone https://github.com/Nats306/TallerEC2---SA_NM.git .
 
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-cp fastapi.service /etc/systemd/system/
+sudo cp fastapi.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start fastapi
 sudo systemctl enable fastapi
