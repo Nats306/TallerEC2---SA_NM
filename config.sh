b5bc99188b12b5f4d2372c6e02f3192336b4f914
sofiaaristizabal <sofiaaristizabal23@gmail.com>
2025-11-05 11:46:28 -0500

    config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ mkdir fastapi
 cd fastapi
 git clone https://github.com/Nats306/TallerEC2---SA_NM.git .
 
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 sudo cp fastapi.service /etc/systemd/system/
